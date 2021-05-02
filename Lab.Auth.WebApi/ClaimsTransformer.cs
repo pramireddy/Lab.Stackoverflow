de01212d5f8e3eababa5f9d6e99524ad5c91ae3f
Prasad Ramireddy <pramireddy08@gmail.com>
2021-05-02 17:56:44 +0100

    Update code fow windosw authentication

---
+++ Lab.Auth.WebApi/ClaimsTransformer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Lab.Auth.WebApi
+{
+    public class ClaimsTransformer : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            //throw new NotImplementedException();
+
+            return Task.FromResult(principal);
+        }
+    }
+}
