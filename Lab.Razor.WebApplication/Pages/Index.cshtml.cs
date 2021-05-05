6870e409720761379fedbcd19282eedafc3e2556
Prasad Ramireddy <pramireddy08@gmail.com>
2021-05-05 17:55:45 +0100

    Added new project for razor pages

---
+++ Lab.Razor.WebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Lab.Razor.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab.Razor.WebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IEnumerable<TestUser> TestUsers;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            TestUsers = new List<TestUser>
+            { 
+                new TestUser { FirstName = "T1", LastName = "T2" },
+                new TestUser { FirstName = "T1", LastName = "T2" },
+                new TestUser { FirstName = "T1", LastName = "T2" },
+                new TestUser { FirstName = "T1", LastName = "T2" },
+                new TestUser { FirstName = "T1", LastName = "T2" },
+                new TestUser { FirstName = "T1", LastName = "T2" },
+                new TestUser { FirstName = "T1", LastName = "T2" }
+
+            };
+        }
+
+       
+    }
+}
---
+++ Lab.Razor.WebApplication/ViewComponents/Route.cs
@@ -0,0 +1,18 @@
+﻿using Lab.Razor.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab.Razor.WebApplication.ViewComponents
+{
+    public class RouteViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(TestUser user)
+        {
+
+            return await Task.FromResult(View(user));
+        }
+    }
+}
